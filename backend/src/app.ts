13531f444bca54096e1bc3147a92d9aa81d93205
MatehsuCoutinho <matheuscoutinho.0420@gmail.com>
2026-03-24 13:48:20 -0300

    chore: remove global requisiton limitter from app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,13 +5,11 @@ import 'dotenv/config';
 import fundRoutes from './routes/fund.routes';
 import transactionRoutes from './routes/transaction.routes';
 import brapiRoutes from './routes/brapi.routes';
-import { globalLimiter } from './middlewares/rateLimiter';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(globalLimiter);
 
 app.get('/health', (_req, res) => {
     res.json({ status: 'ok' });
